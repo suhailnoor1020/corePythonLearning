83ab23e554567d5fe869bcc9afb1664c171b072d
suhail <suhail.noor1020@gmail.com>
2022-10-05 15:36:20 -0400

    test addition of LOCs

---
+++ first.py
@@ -0,0 +1,10 @@
+print("Hello Noor!!")
+print("This is my first python application or program in PyCharm")
+
+print("we are learning Python to be a developer")
+
+print("we are learning Python to be a developer")
+print("we are learning Python to be a developer")
+print("we are learning Python to be a developer")
+print("we are learning Python to be a developer")
+
